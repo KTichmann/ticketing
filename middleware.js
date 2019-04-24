c49c943f5f14c8b06c4c47625440b8c106ba3c4d
unknown <ktichmann@gmail.com>
2019-04-24 17:52:47 +0200

    update middleware to fix bug

--- middleware.js
+++ middleware.js
@@ -1,33 +1,32 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { SECRET } = process.env;
 
 let checkToken = (req, res, next) => {
-    let token = req.headers['x-access-token'] || req.headers['authorization'];
-    if (token.startsWith('Bearer ')){
-        //remove bearer from string
-        token = token.slice(7, token.length);
-    }
-
-    if(token){
-        jwt.verify(token, SECRET, (err, decoded) => {
-            if(err) {
-                return res.json({
-                    success: false,
-                    message: 'Invalid Token'
-                });
-            } else {
-                req.decoded = decoded.username;
-                next();
-            }
-        });
-    } else {
-        return res.json({
-            success: false,
-            message: 'Auth token is not supplied'
-        })
-    }
-}
+	let token = req.headers["x-access-token"] || req.headers["authorization"];
+	if (token) {
+		if (token.startsWith("Bearer ")) {
+			//remove bearer from string
+			token = token.slice(7, token.length);
+		}
+		jwt.verify(token, SECRET, (err, decoded) => {
+			if (err) {
+				return res.json({
+					success: false,
+					message: "Invalid Token"
+				});
+			} else {
+				req.decoded = decoded.username;
+				next();
+			}
+		});
+	} else {
+		return res.json({
+			success: false,
+			message: "Auth token is not supplied"
+		});
+	}
+};
 
 module.exports = {
-    checkToken: checkToken
-}
+	checkToken: checkToken
+};
